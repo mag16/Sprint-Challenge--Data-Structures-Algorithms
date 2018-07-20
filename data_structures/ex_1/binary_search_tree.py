542c3d5c48968df593a3923848e63c011a58b878
Marco A Guzman <marcoguzman16@gmail.com>
2018-07-20 10:25:52 -0700

    fix to DF(value)

--- data_structures/ex_1/binary_search_tree.py
+++ data_structures/ex_1/binary_search_tree.py
@@ -5,7 +5,7 @@ class BinarySearchTree:
     self.right = None
 
   def depth_first_for_each(self, cb):
-    cb(self)
+    cb(self.value)
     if self.left:
       self.left.depth_first_for_each(cb)
     if self.right:
