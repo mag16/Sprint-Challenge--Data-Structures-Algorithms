72958927229c23e904a4f667785ee77ba6e2ae37
Marco A Guzman <marcoguzman16@gmail.com>
2018-07-20 10:01:01 -0700

    DF and BF

--- data_structures/ex_1/binary_search_tree.py
+++ data_structures/ex_1/binary_search_tree.py
@@ -5,10 +5,23 @@ class BinarySearchTree:
     self.right = None
 
   def depth_first_for_each(self, cb):
-    pass    
+    cb(self)
+    if self.left:
+      self.left.depth_first_for_each(cb)
+    if self.right:
+      self.right.depth_first_for_each(cb)
+    return
+    
 
   def breadth_first_for_each(self, cb):
-    pass
+    queue = [self]
+    while len(queue) > 0:
+      current = queue.pop(0)
+      cb(current.value)
+      if self.left:
+        self.left = queue.pop(self.left)
+      if self.right:
+        self.right = queue.pop(self.right)
 
   def insert(self, value):
     new_tree = BinarySearchTree(value)
